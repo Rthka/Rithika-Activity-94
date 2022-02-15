9ac35834b22c6c5455786b6b2cc01cc69d262486
Rthka <97234311+Rthka@users.noreply.github.com>
2022-02-15 18:39:47 +0530

    Add files via upload

---
+++ kwitter_room.js
@@ -0,0 +1,12 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyCNwQFGAV0fiDYrhhmgbAn1R1mBb-aCcCU",
+    authDomain: "rithika-activity-93.firebaseapp.com",
+    databaseURL:"https://rithika-activity-93-default-rtdb.firebaseio.com/",
+    projectId: "rithika-activity-93",
+    storageBucket: "rithika-activity-93.appspot.com",
+    messagingSenderId: "911244628784",
+    appId: "1:911244628784:web:c87ecae32e307dde0a2ef5"
+  };
+  
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
